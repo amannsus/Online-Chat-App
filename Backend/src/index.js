377775833915d20182d0ef1ddcdc7ff5c9739d9b
Aman Kumar Chaudhary <123335986+amannsus@users.noreply.github.com>
2025-08-24 04:31:11 +0530

    Updated chat UI and fixed login modal

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 import { scheduleMessageCleanup } from "./lib/utils.js";
 import authRoutes from "./routes/auth.route.js";
@@ -14,6 +16,8 @@ import groupRoutes from "./routes/group.route.js";
 import handleConnection from "./socket/socketHandler.js";
 
 dotenv.config();
+
+const_dirname = path.resolve();
 
 const app = express();
 const server = http.createServer(app);
@@ -46,6 +50,14 @@ app.use("/api/groups", groupRoutes);
 
 handleConnection(io);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, async () => {
   try {
     await connectDB();
@@ -56,3 +68,5 @@ server.listen(PORT, async () => {
     process.exit(1);
   }
 });
+
+
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,8 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios.js";
 
+ const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/"
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
@@ -88,4 +90,4 @@ export const useAuthStore = create((set, get) => ({
   },
 }));
 
-export default useAuthStore;
+export default useAuthStore;      
